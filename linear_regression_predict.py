7800760b4fda7a607d65fd6741a5dc3f746d47d8
SP Mohanty <sharada.mohanty@epfl.ch>
2017-06-30 02:25:32 +0200

    Add a sample script for predictions by linear regression

---
+++ linear_regression_predict.py
@@ -0,0 +1,37 @@
+import crowdai
+import argparse
+from sklearn import linear_model
+import numpy as np
+
+parser = argparse.ArgumentParser(description='Submit the result to crowdAI')
+parser.add_argument('--api_key', dest='api_key', action='store', required=True)
+args = parser.parse_args()
+
+# Create the challenge object by authentication with crowdAI with your API_KEY
+challenge = crowdai.Challenge("OpenSNPChallenge2017", args.api_key)
+
+#Load training data
+x_train = np.load("data/subset_cm_train.npy")
+y_train = np.load("data/train_heights.npy")
+
+x_test = np.load("data/subset_cm_test.npy")
+
+#Replace nan values in the training and testing set with an arbitrary number
+inds = np.where(np.isnan(x_train))
+x_train[inds] = -100
+inds = np.where(np.isnan(x_test))
+x_test[inds] = -100
+
+
+# Instantiate a linear model
+regr = linear_model.LinearRegression()
+regr.fit(x_train, y_train)
+
+# Predict the heights for the test set
+heights = regr.predict(x_test)
+
+#Convert heights from np.array to a list (to ensure it is JSON serializable)
+heights = heights.tolist()
+
+challenge.submit(heights)
+challenge.disconnect()
